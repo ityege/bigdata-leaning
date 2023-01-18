f0694facd252f0414cfab8e41422747e0ded3a44
ityege <10785180+ityege@user.noreply.gitee.com>
2023-01-18 20:14:31 +0800

    storm hello world 打包上环境运行

--- storm/src/main/java/helloword/ExclamationTopology.java
+++ storm/src/main/java/helloword/ExclamationTopology.java
@@ -1,10 +1,10 @@
 package helloword;
 
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
+//import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.utils.Utils;
+//import org.apache.storm.utils.Utils;
 
 public class ExclamationTopology {
     public static void main(String[] args) throws Exception {
@@ -17,12 +17,13 @@ public class ExclamationTopology {
         if (args != null && args.length > 0) {
             conf.setNumWorkers(3);
             StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
-        } else {
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("test", conf, builder.createTopology());
-            Utils.sleep(20000);
-            cluster.killTopology("test");
-            cluster.shutdown();
         }
+//        else {
+//            LocalCluster cluster = new LocalCluster();
+//            cluster.submitTopology("test", conf, builder.createTopology());
+//            Utils.sleep(20000);
+//            cluster.killTopology("test");
+//            cluster.shutdown();
+//        }
     }
 }
